f78a25a971842af266d317b91f021b827a53ca0d
Hiram Herrera Alcantar <hiramk@cori13.nersc.gov>
2019-11-05 19:34:53 -0800

    Modified fp_subsample.py

--- py/desisim/fp_subsample.py
+++ py/desisim/fp_subsample.py
@@ -15,23 +15,31 @@ from astropy.io import fits
 from astropy.table import Table
 
 def create_subsample_file(cat,outfile=None,nside=16,nest=True):
-    pixels = hp.ang2pix(nside, np.pi/2.-cat['DEC']*np.pi/180.,cat['RA']*np.pi/180.,nest=nest)
-    unique_pixels,qsocount = np.unique(pixels,return_counts=True)
     maxnumobs=max(cat['NUMOBS'])
-    data = np.zeros((unique_pixels.size,2+maxnumobs))
-    data[:,0] = unique_pixels
+    pixels = hp.ang2pix(nside, np.pi/2.-cat['DEC']*np.pi/180.,cat['RA']*np.pi/180.,nest=nest)
     pixarea = hp.pixelfunc.nside2pixarea(nside, degrees=True)
-    density = qsocount/pixarea
-    data[:,1] = density
+    unique_pixels = np.unique(pixels)
+    
+    data = np.zeros((unique_pixels.size,3+maxnumobs))
+    data[:,0] = unique_pixels
+    lowz = (cat['TRUEZ']>=1.8)&(cat['TRUEZ']<2.1)
+    highz= cat['TRUEZ']>2.1
+    
     bins = np.arange(1,2+maxnumobs)
     for i,pix in zip(range(unique_pixels.size),unique_pixels):
-        data[i,2:] = np.histogram(cat['NUMOBS'][pixels==pix],density=True,bins=bins)[0]
+        lowz_qsos=np.count_nonzero(pixels[lowz]==pix)
+        highz_qsos=np.count_nonzero(pixels[highz]==pix)
+        if(lowz_qsos==0 or highz_qsos==0):
+            print(lowz_qsos,highz_qsos,pix)
+        data[i,1]=lowz_qsos/pixarea
+        data[i,2]=highz_qsos/pixarea
+        data[i,3:] = np.histogram(cat['NUMOBS'][highz][pixels[highz]==pix],density=True,bins=bins)[0]
     header = 'DESI YEAR 5 HIGH Z QSOS FOOTPRINT \n\n'
     header+= 'NSIDE = {} \n'.format(nside)
     header+= 'NEST = {} \n'.format(nest)
-    header+= f'pixel_index density [nb/deg^{2}] numobs_probability {list(bins[:-1])}\n'
+    header+= f'pixel_index lowz_density [nb/deg^{2}] highz_density [nb/deg^{2}] numobs_probability {list(bins[:-1])}\n'
     if outfile:
-        np.savetxt(outfile,data,fmt='%u '+'%.5e '+'%.8e '*maxnumobs, header=header)
+        np.savetxt(outfile,data,fmt='%u '+'%.5e '+'%.5e '+'%.8e '*maxnumobs, header=header)
         print(f'Saved {outfile} file')
         return
     else:
@@ -51,8 +59,8 @@ class footprint_subsample:
             raise ValueError('file with DESI subsample footprint does not exist')
         data = np.loadtxt(fname)
         pix = data[:,0].astype(int)
-        dens = data[:,1]
-        numobs_prob = data[:,2:]
+        dens = data[:,1:2]
+        numobs_prob = data[:,3:]
         return pix,dens,numobs_prob
     
     def lyaqsos_density(self,ra,dec):
@@ -80,7 +88,7 @@ def dataset_subsample(ra,dec,input_density,subsample_footprint,nside):
         selection (ndarray): mask to apply to downsample input list
     """
     # figure out expected DESI subsample footprint density, 
-    # in quasars / sq.deg., at high-z
+    # in quasars / sq.deg.
     N=len(ra)
     density = subsample_footprint.lyaqsos_density(ra,dec)
     pixarea = hp.pixelfunc.nside2pixarea(nside, degrees=True)
