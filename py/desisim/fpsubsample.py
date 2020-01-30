aedf6d428d768898b908976ff953fbfea42dcf40
Hiram Herrera Alcantar <hiramk@cori13.nersc.gov>
2020-01-30 10:55:28 -0800

    Modified fp_subsample.py name to fpsubsample.py

--- py/desisim/fpsubsample.py
+++ py/desisim/fpsubsample.py
@@ -12,7 +12,7 @@ import healpy
 from pkg_resources import resource_filename
 import healpy as hp
 from astropy.io import fits
-from astropy.table import Table
+from astropy.table import Table, Column
 
 def create_subsample_file(cat,outfile=None,nside=16,nest=True):
     maxnumobs=max(cat['NUMOBS'])
@@ -21,55 +21,59 @@ def create_subsample_file(cat,outfile=None,nside=16,nest=True):
     unique_pixels = np.unique(pixels)
     
     data = np.zeros((unique_pixels.size,3+maxnumobs))
-    data[:,0] = unique_pixels
-    lowz = (cat['TRUEZ']>=1.8)&(cat['TRUEZ']<2.1)
-    highz= cat['TRUEZ']>2.1
+    
+    # REDSHIFT MASKS
+    lowz = cat['TRUEZ']<1.8
+    midz = (cat['TRUEZ']>=1.8)&(cat['TRUEZ']<2.1)
+    highz= cat['TRUEZ']>=2.1
     
     bins = np.arange(1,2+maxnumobs)
-    for i,pix in zip(range(unique_pixels.size),unique_pixels):
-        lowz_qsos=np.count_nonzero(pixels[lowz]==pix)
+    for i,pix in enumerate(unique_pixels):
+        lowz_qsos = np.count_nonzero(pixels[lowz]==pix)
+        midz_qsos=np.count_nonzero(pixels[midz]==pix)
         highz_qsos=np.count_nonzero(pixels[highz]==pix)
-        if(lowz_qsos==0 or highz_qsos==0):
-            print(lowz_qsos,highz_qsos,pix)
-        data[i,1]=lowz_qsos/pixarea
+        data[i,0]=lowz_qsos/pixarea
+        data[i,1]=midz_qsos/pixarea
         data[i,2]=highz_qsos/pixarea
         data[i,3:] = np.histogram(cat['NUMOBS'][highz][pixels[highz]==pix],density=True,bins=bins)[0]
-    header = 'DESI YEAR 5 HIGH Z QSOS FOOTPRINT \n\n'
-    header+= 'NSIDE = {} \n'.format(nside)
-    header+= 'NEST = {} \n'.format(nest)
-    header+= f'pixel_index lowz_density [nb/deg^{2}] highz_density [nb/deg^{2}] numobs_probability {list(bins[:-1])}\n'
+        
+    # CREATE A TABLE FROM DATA
+    colnames = ['LOWZ_DENS','MIDZ_DENS','HIGHZ_DENS']+[f'PROB_NUMOBS_{i}' for i in range(1,maxnumobs+1)]
+    tbl = Table(data,names=colnames)
+    tbl.add_column(unique_pixels.astype(int),index=0,name='HPXPIXEL')
+    tbl.meta['name'] = 'DESI YEAR 5 HIGH Z QSOS FOOTPRINT'
+    tbl.meta['NSIDE'] = nside
+    tbl.meta['NEST'] = nest
     if outfile:
-        np.savetxt(outfile,data,fmt='%u '+'%.5e '+'%.5e '+'%.8e '*maxnumobs, header=header)
+        tbl.write(outfile,overwrite=True)
         print(f'Saved {outfile} file')
         return
     else:
-        return data
+        return tbl
     
 class footprint_subsample:
     def __init__(self,fname,nside):
         self.nside = nside
         print('DESI subsample, nside',nside)
-        self.subsample_pix,self.subsample_dens,self.numobs_prob = self.read_footprint_subsample(fname,self.nside)
+        self.subsample_pix,self.subsample_dens,self.numobs_prob = self.read_footprint_subsample(fname)
         print('got data from file')
     
-    def read_footprint_subsample(self,fname,nside):
+    def read_footprint_subsample(self,fname):
         print('Reading subsample footprint from file',fname)
         if not os.path.isfile(fname):
             print('DESI subsample file',fname)
             raise ValueError('file with DESI subsample footprint does not exist')
-        data = np.loadtxt(fname)
-        pix = data[:,0].astype(int)
-        dens = {'LOWZ':data[:,1],'HIGHZ':data[:,2]}
-        numobs_prob = data[:,3:]
-        return pix,dens,numobs_prob
+        data = Table.read(fname)
+        pix = data['HPXPIXEL']
+        dens = data['LOWZ_DENS','MIDZ_DENS','HIGHZ_DENS']
+        prob_numobs = data['PROB_NUMOBS']
+        return pix,dens,prob_numobs
     
     def lyaqsos_density(self,ra,dec):
         pixs = hp.ang2pix(self.nside, np.pi/2.-dec*np.pi/180.,ra*np.pi/180.,nest=True)
         thispix = np.unique(pixs)
-        index = np.where(self.subsample_pix==thispix) 
-        dens=dict()
-        dens['LOWZ']=self.subsample_dens['LOWZ'][index]
-        dens['HIGHZ']=self.subsample_dens['HIGHZ'][index]
+        index = np.where(self.subsample_pix==thispix)[0] 
+        dens=self.subsample_dens[index]
         return dens
     
     def lyaqsos_obsprob(self,ra,dec):
@@ -79,9 +83,9 @@ class footprint_subsample:
         probs = self.numobs_prob[index]
         return probs
 
-def dataset_subsample(ra,dec,Z,subsample_footprint,nside):
+def dataset_subsample(ra,dec,Z,subsample_footprint):
     """ Downsample input list of angular positions based on DESI's year 5 footprint
-        and input density of all quasars .
+        and input density of all quasars.
     Args:
         ra (ndarray): Right ascension (degrees)
         dec (ndarray): Declination (degrees)
@@ -91,14 +95,20 @@ def dataset_subsample(ra,dec,Z,subsample_footprint,nside):
     """
     # figure out expected DESI subsample footprint density, 
     N=len(ra)
+    nside = subsample_footprint.nside
     density = subsample_footprint.lyaqsos_density(ra,dec)
+    
+    if len(density)==0: # Return empty array because there is no intersection with subsample footprint
+        return np.array([])
+        
     pixarea = hp.pixelfunc.nside2pixarea(nside, degrees=True)
     selection=dict()
-    index={'LOWZ':np.where(Z<2.1)[0],'HIGHZ':np.where(Z>=2.1)[0]}
-    for whichz in ['LOWZ','HIGHZ']:
-        if len(density[whichz])==0: # Return empty array because there is no intersection with subsample footprint
-            return np.array([])
-        thisN=int(density[whichz]*pixarea)
+    index={'LOWZ':np.where(Z<1.8)[0],'MIDZ':np.where((Z>=1.8)&(Z<2.1))[0],'HIGHZ':np.where(Z>=2.1)[0]}
+    for whichz in ['LOWZ','MIDZ','HIGHZ']:
+        if len(index[whichz])==0:
+            selection[whichz]= np.array([],dtype=int)
+            continue
+        thisN=int(density[f'{whichz}_DENS']*pixarea)
         if len(index[whichz])<thisN:
             thisN=len(index[whichz])
             print(f'Not enough {whichz} quasars in metadata, taking {thisN} availables')
@@ -106,7 +116,7 @@ def dataset_subsample(ra,dec,Z,subsample_footprint,nside):
         whichIndeces=np.random.choice(index[whichz],size=thisN,replace=False)
         selection[whichz]=whichIndeces
         print(len(selection[whichz]),f'{whichz} quasars selected out of',len(index[whichz]))
-    return np.concatenate((selection['LOWZ'],selection['HIGHZ']))
+    return np.concatenate((selection['LOWZ'],selection['MIDZ'],selection['HIGHZ']))
 
 def dataset_exptime(ra,dec,Z,subsample_footprint):
     """ Array of random observation times based on probabilities on file.
@@ -117,10 +127,11 @@ def dataset_exptime(ra,dec,Z,subsample_footprint):
         exptime (ndarray): array of exposure time for each quasar.
     """
     N=len(ra)
-    obs_prob = subsample_footprint.lyaqsos_obsprob(ra,dec)
-    numobs = np.arange(1,len(obs_prob)+1)
-    exptime = 1000*np.random.choice(numobs,size=N,p=obs_prob)
+    prob_numobs = subsample_footprint.lyaqsos_obsprob(ra,dec)
+    numobs = np.arange(1,len(prob_numobs)+1)
+    exptime = 1000*np.random.choice(numobs,size=N,p=prob_numobs)
     exptime[Z<2.1]=1000.
-    print(f'Generated {len(exptime[Z<2.1])} Low-z qsos with 1000 exposure time')
-    print(f'Generated {len(exptime[Z>=2.1])} High-z qsos with {numobs} exposures with probabilities {obs_prob}')
+    print(f'Generated {len(exptime[Z<1.8])} Low-z qsos with 1000 exposure time')
+    print(f'Generated {len(exptime[(Z>=1.8)&(Z<2.1)])} Mid-z qsos with 1000 exposure time')
+    print(f'Generated {len(exptime[Z>=2.1])} High-z qsos with {numobs} exposures with probabilities {prob_numobs}')
     return exptime
