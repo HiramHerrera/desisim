7ae0765a190be4fa429e89c23672127708497943
HiramHerrera <hk.herreraalcantar@ugto.mx>
2021-08-13 15:18:53 -0700

    Deprecated LOW redshift distribution

--- py/desisim/fpsubsample.py
+++ py/desisim/fpsubsample.py
@@ -14,61 +14,7 @@ import healpy as hp
 from astropy.io import fits
 from astropy.table import Table, Column
 
-def create_density_file(cat,outfile=None,nside=16,nest=True):
-    """Create a density file containing HPXPIXEL, quasar density,
-    and number of observation probability.
-    
 
-    Args:
-        cat (path): Input quasar catalog
-        
-    Optional:
-        outfile (path): Output density file path, (default==None).
-        nside (int): NSIDE for the sky (default==16)
-        nest (bool): NEST for the sky (default==True)
-        
-    Returns:
-        If outfile option is set None: Table object with the density 
-        and number of observations probability information.
-        If outfile option is given: None
-    """
-    maxnumobs=max(cat['NUMOBS'])
-    pixels = hp.ang2pix(nside, np.pi/2.-cat['DEC']*np.pi/180.,cat['RA']*np.pi/180.,nest=nest)
-    pixarea = hp.pixelfunc.nside2pixarea(nside, degrees=True)
-    unique_pixels = np.unique(pixels)
-    
-    # GENERATE TABLE AND TABLE META
-    colnames = ['HPXPIXEL','LOWZ_DENS','MIDZ_DENS','HIGHZ_DENS']
-    tbl = Table(names=colnames,dtype=('i8','f8','f8','f8'))
-    tbl.meta['name'] = 'DESI YEAR 5 QSOS FOOTPRINT'
-    tbl.meta['comments'] =['This table contains information about the QSO density for each pixel']
-    tbl.meta['NSIDE'] = nside
-    tbl.meta['NEST'] = nest
-    
-    # REDSHIFT MASKS
-    lowz = cat['TRUEZ']<1.8
-    midz = (cat['TRUEZ']>=1.8)&(cat['TRUEZ']<2.1)
-    highz= cat['TRUEZ']>=2.1
-
-    
-    bins = np.arange(1,2+maxnumobs)
-    prob_numobs=np.zeros((unique_pixels.size,maxnumobs))
-    for i,pix in enumerate(unique_pixels):
-        lowz_dens = np.count_nonzero(pixels[lowz]==pix)/pixarea
-        midz_dens = np.count_nonzero(pixels[midz]==pix)/pixarea
-        highz_dens= np.count_nonzero(pixels[highz]==pix)/pixarea
-        
-        prob_numobs[i]=np.histogram(cat['NUMOBS'][highz][pixels[highz]==pix],density=True,bins=bins)[0]
-        tbl.add_row((pix,lowz_dens,midz_dens,highz_dens))
-    tbl.add_column(Column(prob_numobs,name='PROB_NUMOBS'))  
-
-    if outfile:
-        tbl.write(outfile,overwrite=True)
-        print(f'Saved {outfile} file')
-        return
-    else:
-        return tbl
-    
 class footprint_subsample:
     """Class to store useful functions to create a subsample footprint with\
     multiple exposure times.
@@ -101,7 +47,7 @@ class footprint_subsample:
             raise ValueError('NEST option from density file does not match NEST from transmission file')
             
         pix = data['HPXPIXEL']
-        dens = data['LOWZ_DENS','MIDZ_DENS','HIGHZ_DENS']
+        dens = data['MIDZ_DENS','HIGHZ_DENS']
         prob_numobs = data['PROB_NUMOBS']
         return pix,dens,prob_numobs
     
@@ -130,9 +76,9 @@ def dataset_subsample(Z,subsample_footprint):
     
     pixarea = hp.pixelfunc.nside2pixarea(nside, degrees=True)
     selection=np.array([],dtype=int)
-    index={'LOWZ':np.where(Z<1.8)[0],'MIDZ':np.where((Z>=1.8)&(Z<2.1))[0],'HIGHZ':np.where(Z>=2.1)[0]}
+    index={'MIDZ':np.where((Z>=1.8)&(Z<2.1))[0],'HIGHZ':np.where(Z>=2.1)[0]}
     if len(density)!=0:
-        for whichz in ['LOWZ','MIDZ','HIGHZ']:
+        for whichz in ['MIDZ','HIGHZ']:
             thisN=np.round(density[f'{whichz}_DENS']*pixarea).astype(int)
             if len(index[whichz])==0 or thisN==0:
                 continue   
